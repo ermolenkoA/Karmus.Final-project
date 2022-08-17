de0c41e496297731eda00708d282315fb6a126c1
Ermolenko Artem <kypalo2012@gmail.com>
2022-08-17 14:01:44 +0300

    Add sing up button on main screen

---
+++ Karmus/Karmus/Utils/Constants/ConstantReferences.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantReferences.swift
+//  Karmus
+//
+//  Created by User on 8/17/22.
+//
+
+import Foundation
+
+public struct References {
+    static let fromMainToIdentificationScreen = "toIdentificationScreen"
+    static let fromMainToRegistrationScreen = "toRegistrationScreen"
+    static let fromIdentificationToRegistrationScreen = "toRegistrationScreen2"
+}
